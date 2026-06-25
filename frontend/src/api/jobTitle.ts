ac4a1794f1cdaeb048726c37a02baf2d360c3496
Letrongtri <trib2206019@student.ctu.edu.vn>
2026-06-25 21:36:56 +0700

    [fe] add fetch job titles

--- frontend/src/api/jobTitle.ts
+++ frontend/src/api/jobTitle.ts
@@ -1,7 +1,24 @@
 import axiosClient from './axios'
-import type { JobTitleSimple } from '../types/jobTitle'
+import type {
+    JobTitlePaginatedResponse,
+    JobTitleQuery,
+    JobTitleSimple,
+} from '../types/jobTitle'
 
 export const fetchAllSimpleJobTitles = async (): Promise<JobTitleSimple[]> => {
     const res = await axiosClient.get('/job_titles/simple')
     return res.data
+}
+
+export const fetchJobTitles = async (
+    query: JobTitleQuery
+): Promise<JobTitlePaginatedResponse> => {
+    const response = await axiosClient.get(`/job_titles`, {
+        params: {
+            query: query.query,
+            page: query.page,
+            limit: query.limit,
+        },
+    })
+    return response.data
 }
--- frontend/src/types/jobTitle.ts
+++ frontend/src/types/jobTitle.ts
@@ -1,7 +1,27 @@
-import type { ID } from './common'
+import type { ID, Timestamp, PaginatedResponse } from './common'
 
 export interface JobTitleSimple {
     id: ID
     title_name: string
     code: string
+}
+
+export interface JobTitle {
+    id: ID
+    title_name: string
+    code: string
+    description?: string
+    created_at: Timestamp
+    updated_at: Timestamp
+}
+
+export interface JobTitleQuery {
+    query?: string | null
+    page?: number | null
+    limit?: number
+}
+
+export interface JobTitlePaginatedResponse {
+    job_titles: JobTitle[]
+    pagination: PaginatedResponse
 }
