10e4718119eb5be6565d620812da6c23a718a0a1
Letrongtri <trib2206019@student.ctu.edu.vn>
2026-06-25 21:36:28 +0700

    [fe] add fetch departments

--- frontend/src/api/department.ts
+++ frontend/src/api/department.ts
@@ -1,9 +1,26 @@
 import axiosClient from './axios'
-import type { DepartmentSimple } from '../types/department'
+import type {
+    DepartmentPaginatedResponse,
+    DepartmentQuery,
+    DepartmentSimple,
+} from '../types/department'
 
 export const fetchAllSimpleDepartments = async (): Promise<
     DepartmentSimple[]
 > => {
     const res = await axiosClient.get('/departments/simple')
     return res.data
+}
+
+export const fetchDepartments = async (
+    query: DepartmentQuery
+): Promise<DepartmentPaginatedResponse> => {
+    const response = await axiosClient.get(`/departments`, {
+        params: {
+            query: query.query,
+            page: query.page,
+            limit: query.limit,
+        },
+    })
+    return response.data
 }
--- frontend/src/types/department.ts
+++ frontend/src/types/department.ts
@@ -1,7 +1,27 @@
-import type { ID } from './common'
+import type { ID, Timestamp, PaginatedResponse } from './common'
 
 export interface DepartmentSimple {
     id: ID
     name: string
     code: string
+}
+
+export interface Department {
+    id: ID
+    name: string
+    code: string
+    description?: string
+    created_at: Timestamp
+    updated_at: Timestamp
+}
+
+export interface DepartmentQuery {
+    query?: string | null
+    page?: number | null
+    limit?: number
+}
+
+export interface DepartmentPaginatedResponse {
+    departments: Department[]
+    pagination: PaginatedResponse
 }
